8a89ca71c327348b3e69467fa655c403d8500f77
firs <firs058@gmail.com>
2020-10-02 00:09:06 +0500

    3.70.14

--- src/main/java/org/myongoingscalendar/model/InputUserValues.java
+++ src/main/java/org/myongoingscalendar/model/InputUserValues.java
@@ -2,8 +2,10 @@ package org.myongoingscalendar.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class InputUserValues {
     private Integer tid;
